5e939d7cba2ed66202ee43daf7048c7661187b48
emilykidd3 <e.kidd61@yahoo.com>
2022-03-27 17:44:46 -0700

    added div to html to hold game board and js to dynamially create squares

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
+document.addEventListener('DOMContentLoaded', () => 
+    function makeGameBorad() {
+        var grid = document.querySelector(".grid")
+
+        for (var i = 0; i < 200; i ++) {
+            let square = document.createElement("div");
+            gameBoard.appendChild(square);
+        }
+    }
 })
