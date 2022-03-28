f0b3633d822529857b1ba209ac507df927d9b08e
emilykidd3 <e.kidd61@yahoo.com>
2022-03-27 18:06:36 -0700

    added dom references in js and array for divs

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function makeGameBoard() {
-        var grid = document.querySelector(".grid");
+    var grid = document.querySelector(".grid");
+    const ScroeDisplay = document.querySelector('#score');
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+    const height = 20
 
+    function makeGameBoard() {
+        var squares = []
         for (var i = 0; i < 200; i ++) {
             let square = document.createElement("div");
             grid.appendChild(square);
+            squares.push(square);
         }
+        console.log(squares)
     }
 
     makeGameBoard();
