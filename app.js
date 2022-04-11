65f2f24a60692978d2a9210602afc377721f0b87
emilykidd3 <e.kidd61@yahoo.com>
2022-04-10 22:16:47 -0700

    added add score function to remove row when filled and add score to page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 10
     let nextRandom = 0;
     let timerId
+    let score = 0;
 
     // the shapes
     const ltetromino = [
@@ -124,8 +125,9 @@ document.addEventListener('DOMContentLoaded', () => {
             nextRandom = Math.floor(Math.random() * theTetrominoes.length)
             current = theTetrominoes[random][currentRotation]
             currentPosition = 4
-            draw()
-            displayShape()
+            draw();
+            displayShape();
+            addScore();
         }
     }
 
@@ -200,4 +202,22 @@ document.addEventListener('DOMContentLoaded', () => {
             displayShape()
         }
     })
+
+    function addScore() {
+        for (let i = 0; i < 199; i+= width){
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+            if (row.every(index => squares[index].classList.contains('taken'))) {
+                score += 10
+                scroeDisplay.innerHTML = score
+                row.forEach(index => {
+                    squares[index].classList.remove('taken')
+                    squares[index].classList.remove('tetromino')
+                })
+                const squaresRemoved = squares.splice(i, width)
+                squares = squaresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
+        }
+    }
 })
